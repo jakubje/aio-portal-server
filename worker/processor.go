1a646ceb90a156e9945f81c844c1dcff40dd3f76
jakubje <jakubjeczalik@gmail.com>
2023-08-04 17:02:09 +0100

    Added Redis and asynq logger

--- worker/processor.go
+++ worker/processor.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"github.com/hibiken/asynq"
 	db "github.com/jakub/aioportal/server/db/sqlc"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -22,6 +24,9 @@ type RedisTaskProcessor struct {
 }
 
 func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	logger := NewLogger()
+	redis.SetLogger(logger)
+
 	server := asynq.NewServer(
 		redisOpt,
 		asynq.Config{
@@ -29,6 +34,11 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
+			Logger: logger,
 		},
 	)
 
